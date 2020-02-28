fefd2fe7efad8c8172fe06bd222025046b1df66a
LiuJingwei0420 <58125786+LiuJingwei0420@users.noreply.github.com>
2020-02-28 09:39:10 +0800

    modify the tabbar

--- src/main.js
+++ src/main.js
@@ -1,22 +1,36 @@
 //入口文件
 console.log('ok')
 import Vue from 'vue'
+//1.1 导入路由的包
+import VueRouter from 'vue-router'
+//1.2 安装路由
+Vue.use(VueRouter)
+
 
 //导入 MUI 的样式
 import './lib/mui/css/mui.min.css'
-// import MintUI from 'mint-ui'
-// import 'mint-ui/lib/style.css'
-// Vue.use(MintUI)  
+import './lib/mui/css/icons-extra.css'
+ 
 // git push origin master
 // 上面的是指令
 
-
+// 按需导入 Mint-UI 中的组件
 import 'mint-ui/lib/style.css'
-import { Header } from 'mint-ui'
+import { Header,Swipe, SwipeItem } from 'mint-ui'
+
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
 //导入 APP 根组件
 import app from './App.vue'
+
+
+// 1.3 导入自己的 router.js 路由模块
+import router from './router.js'
+
 var vm = new Vue({
     el: '#app',
-    render: c => c(app)
+    render: c => c(app),
+    router // 1.4 挂载路由对象到实例上
 })
